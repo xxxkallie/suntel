13593da6195dfdd97974e263182cdf529c48a229
Hopa Sergiusz <pd3664@pjwstk.edu.pl>
2024-06-15 17:55:49 +0200

    New

---
+++ src/main/java/com/hotel/suntel1/exception/PhotoRetrievalException.java
@@ -0,0 +1,8 @@
+package com.hotel.suntel1.exception;
+
+
+public class PhotoRetrievalException extends RuntimeException {
+    public PhotoRetrievalException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hotel/suntel1/exception/RoleAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.hotel.suntel1.exception;
+
+
+
+public class RoleAlreadyExistException extends RuntimeException {
+    public RoleAlreadyExistException(String message) {
+        super(message);
+    }
+}
